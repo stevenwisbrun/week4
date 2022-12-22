549c54b06f4b5dfb68cd0fc49c7011735aef0f77
Steven Wisbrun <stevenwisbrun@hotmail.com>
2022-12-22 15:56:06 +0100

    new scripts

---
+++ scripts/changeX.js
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+
+const CONTRACT_ADDRESS = "0x91Fb63bBe14251d9DC49399D434497b8d528631b";
+
+async function main() {
+  const contract = await hre.ethers.getContractAT("Contract", CONTRACT_ADDRESS);
+
+  await contract.deployed();
+
+  console.log(await contract.changeX(41));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
